3d8f858d8d15a213e4a3623f3fd71871691cffb5
Mohammed AbuShallouf <109415965+Mohd2040@users.noreply.github.com>
2025-06-06 15:46:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+app.use(express.static('public')); 
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
